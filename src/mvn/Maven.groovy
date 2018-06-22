e478218392880b8468857130ecf4623534f30e9a
shambanna <shambanna.u@kloud9.nyc>
2018-06-22 11:34:54 +0530

    Update Maven.groovy

--- src/mvn/Maven.groovy
+++ src/mvn/Maven.groovy
@@ -1,1 +1,11 @@
+package mvn;
 
+class Maven implements Serializable{
+ def script; 
+  Maven(script){
+    this.script = script;
+  }
+  void test(){
+  this.script.sh "mvn clean install -f getting-started-java/helloworld-jsp/"
+  }
+}
