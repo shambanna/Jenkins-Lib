1a2abbfed239420123ae0bd5c7e62ae0fdce0e66
shambanna <shambanna.u@kloud9.nyc>
2018-06-23 08:57:34 +0530

    Update ReadYaml.groovy

--- src/com/ReadYaml.groovy
+++ src/com/ReadYaml.groovy
@@ -9,9 +9,7 @@ class ReadYaml implements Serializable{
   }
   void test(){
   def yaml = this.script.libraryResource "vm.yaml"
-  String path = "../"+this.script.env.JOB_NAME+"@libs/shared-lib/resources/vm.yaml"
-  this.script.println path
-  appBuild = new Yaml().load("../../"+this.script.env.JOB_NAME+"@libs/shared-lib/resources/vm.yaml")
+  appBuild = new Yaml().load(yaml)
   this.script.println appBuild
 
   }
