40bb39034d09368200d0d6b94e03ad5c0920c0e6
shambanna <shambanna.u@kloud9.nyc>
2018-06-23 08:32:11 +0530

    Update Yaml.groovy

--- src/com/Yaml.groovy
+++ src/com/Yaml.groovy
@@ -8,12 +8,8 @@ class Yaml implements Serializable{
     this.script = script;
   }
   void test(){
-  this.script.sh "ls -al"
-  this.script.sh "pwd"
   def yaml = this.script.libraryResource "vm.yaml"
-  this.script.println yaml
   appBuild = new Yaml().load(this.script.libraryResource("vm.yaml"))
-  //List projects = new Yaml().load(("resources/vm.yaml" as File).text)
   this.script.println appBuild
 
   }
